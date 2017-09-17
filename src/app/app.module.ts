a6f4052422b42c31ebfb29dab18b190b4f217cec
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-09-17 13:46:57 -0400

    adding npm module ng2-scroll-to for smooth scrolling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {ScrollToModule} from 'ng2-scroll-to';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
