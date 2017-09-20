048dad63503614e1b0be246b6616a94255015667
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-09-20 09:34:29 -0400

    updates - adding direct source and vcn under projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,8 @@ export class AppComponent {
   my_photo = '/assets/images/amit_photo.jpg';
   flask_image = '/assets/images/projects/flask.png';
   angular2_cli = '/assets/images/projects/angular2cli.png';
+  vcn_image = '/assets/images/projects/vcn.png';
+  directsource_image = '/assets/images/projects/directsource.png';
 
   /*flask_image = '/assets/images/projects/1.png';
   angular2_cli = '/assets/images/projects/2.png';*/
