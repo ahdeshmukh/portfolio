b169c4e451801586867cf09291357b743365eab0
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-09-20 21:38:36 -0400

    adding resume in pdf format available for download

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   angular2_cli = '/assets/images/projects/angular2cli.png';
   vcn_image = '/assets/images/projects/vcn.png';
   directsource_image = '/assets/images/projects/directsource.png';
+  resume = '/assets/resume.pdf';
 
   /*flask_image = '/assets/images/projects/1.png';
   angular2_cli = '/assets/images/projects/2.png';*/
