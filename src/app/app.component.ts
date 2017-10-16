12357612af547ef077a1beea26dce4b934083f19
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-10-16 16:39:39 -0400

    adding a responsive navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,17 +19,7 @@ export class AppComponent {
   angular2_cli = '/assets/images/projects/2.png';*/
 
   ngOnInit(): void {
-    $(document).ready(function(){
-	$(window).on("scroll",function(){
-  	var wn = $(window).scrollTop();
-    if(wn > 120){
-    	$(".navbar-default").css("background","rgba(255,0,0,1)");
-    }
-    else{
-    	$(".navbar-default").css("background","rgba(1,1,1,1)");
-    }
-  });
-});
+    
   }
 
 
